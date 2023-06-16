70cdb00a32bc8ccf4344a7ef9245fe418362e432
Billy <billyrumbiak19@gmail.com>
2023-06-16 10:54:55 +0900

    update terbaru

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php
-    // phpinfo();
-    $txt = "PHP";
-    echo "I love $txt!";
-?>
+    <?php
+
+        include_once('include/header.php');
+        // Nama Lengkap
+        $namaDepan = 'Billy';
+        $namaBelakang = 'Rumbiak';
+
+        $textWelcome = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, quas?';
+        $textSubTitle = 'Belajar PHP Day 01';
+
+    ?>
+
+    <!-- Hello -->
+    <h1>Hello <?php echo $namaDepan .' '.$namaBelakang?> </h1>
+    <h2> <?= $textWelcome ?> </h2>
+    <p> <?= $textSubTitle ?> </p>
+
+    <a href="home.php">Home</a>
+
+    <?php include_once('include/footer.php') ?>
